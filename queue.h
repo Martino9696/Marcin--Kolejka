d829a2e9e621af3f3383e4497b26c789ae65f714
Marcino9696 <252884@edu.p.lodz.pl>
2024-05-31 21:04:52 +0200

    initial commit

---
+++ queue.h
@@ -0,0 +1,21 @@
+//
+// Created by kopyt on 31.05.2024.
+//
+
+#ifndef UNTITLED66_QUEUE_H
+#define UNTITLED66_QUEUE_H
+
+#define QUEUE_SIZE 5
+
+int isQueueEmpty(void);
+
+int isQueueFull(void);
+
+void Enqueue(int val);
+
+int Dequeue(void);
+
+int Front(void);
+
+#endif //UNTITLED66_QUEUE_H
+
